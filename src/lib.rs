dba19bb9945b29bba68b10fc5655da6d380f2ba8
Deepanshu Arora <43300955+Deepansharora27@users.noreply.github.com>
2022-03-14 21:06:12 +0530

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+#![crate_type = "lib"]
+#![crate_name = "testaso"]
+#![cfg(test)]
+
+/// This macro enables writing tests for alignment, size, and offset of fields in structs.
+/// Example usage:
+/// testaso! {
+///     struct mystruct: 8, 4096 => {
+///         f1: 0,
+///         f2: 8
+///     }
+/// }
+#[macro_export]
+#[allow(unused)]
+#[cfg(test)]
+
+macro_rules! testaso_macro {
+    (@off $name:path=>$field:ident) => {
+        memoffset::offset_of!($name, $field)
+    };
+
+    ($(struct $name:path: $align:expr, $size:expr => { $($field:ident: $offset:expr),* })+) => {
+        #[cfg(test)]
+        #[test]
+        pub fn align() {
+            use core::mem::align_of;
+
+            $(
+                assert_eq!(
+                    align_of::<$name>(),
+                    $align,
+                    "align: {}",
+                    stringify!($name)
+                );
+            )+
+        }
+
+        #[cfg(test)]
+        #[test]
+        pub fn size() {
+            use core::mem::size_of;
+
+            $(
+                assert_eq!(
+                    size_of::<$name>(),
+                    $size,
+                    "size: {}",
+                    stringify!($name)
+                );
+            )+
+        }
+
+        #[cfg(test)]
+        #[test]
+        pub fn offsets() {
+            $(
+                $(
+                    assert_eq!(
+                        testaso!(@off $name=>$field),
+                        $offset,
+                        "offset: {}::{}",
+                        stringify!($name),
+                        stringify!($field)
+                    );
+                )*
+        )+
+        }
+    };
+}
